64cfad54ba7c1e049a0f00490bb219529a42d0a1
sifii <sifii2013@gmail.com>
2017-02-02 12:03:11 +0600

    added rotate, resize

--- image.py
+++ image.py
@@ -1,12 +1,22 @@
 from PIL import Image 
 
 # open colour image
-image_file = Image.open("image.jpg") 
+image_file = Image.open("image.jpg")
 
 # convert image to black and white
-image_file = image_file.convert('1') 
+black_and_white = image_file.convert('1')
 
-#another way you can rewrite this line
-#image_file = Image.open("image.jpg").convert("L")
+#   another way you can rewrite this line
+#   image_file = Image.open("image.jpg").convert("L")
+black_and_white.save('black_and_white.jpg')
 
-image_file.save('image.jpg')
+#   resize the image
+size = (50, 150)    # width, height
+new_image = image_file.resize(size, 1)
+new_image.save('resize2.jpg')
+
+
+#   rotate the image
+rotate_image = image_file.rotate(90)
+rotate_image.save('rotate_image.jpg')
+
